d94a5eb4dbeecfc5f7fa02e53a83d30cb1377c62
Rino Jose <rjose@linkedin.com>
2023-05-08 10:26:53 -0700

    WORKAROUND for urllib3 v2.0 ssl error

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     # readthedocs builds fail unless zip_safe is False.
     zip_safe=False,
     install_requires=[
+        "urllib3==1.26.10",
         "pytz",
         "cryptography",
         "python-dateutil",
