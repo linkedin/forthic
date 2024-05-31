970e944b6ac6fc97c98bc7af9bdcc8b20b5ca960
Rino Jose <rjose@forthix.com>
2024-05-31 08:13:32 -0700

    Fix: Escape '.' correctly in regex

--- forthic-react/v1/update_template.py
+++ forthic-react/v1/update_template.py
@@ -1,27 +1,29 @@
 import re
 import glob
 
+
 def main():
     template_path = "../../server/templates/react/react-app/v1/main.html"
 
     # Get new js and css filenames
     [js_path] = glob.glob("./build/static/js/main*.js")
     [css_path] = glob.glob("./build/static/css/main*.css")
-    js_file = re.match(".*(main\..*\.js)", js_path).group(1)
-    css_file = re.match(".*(main\..*\.css)", css_path).group(1)
+    js_file = re.match(".*(main\\..*\\.js)", js_path).group(1)
+    css_file = re.match(".*(main\\..*\\.css)", css_path).group(1)
 
     # Load current template
     with open(template_path) as file:
         contents = file.read()
 
     # Replace current compiled files with new ones
-    contents = re.sub("main\..*\.js", js_file, contents)
-    contents = re.sub("main\..*\.css", css_file, contents)
+    contents = re.sub("main\\..*\\.js", js_file, contents)
+    contents = re.sub("main\\..*\\.css", css_file, contents)
 
     # Update file
     with open(template_path, "w") as file:
         file.write(contents)
     return
+
 
 if __name__ == "__main__":
     main()
