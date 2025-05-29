dd84e4145e2a89df83026313d22e8885835502ab
Rino Jose <rjose@forthix.com>
2025-05-29 08:49:00 -0700

    Bump version to v0.16.1
    - Export utility functions

--- forthic-ts/src/index.ts
+++ forthic-ts/src/index.ts
@@ -2,3 +2,4 @@ export * from "./forthic/interpreter.js";
 export * from "./forthic/tokenizer";
 export * from "./forthic/module.js";
 export * from "./forthic/errors.js"
+export * from "./forthic/utils.js"
